c2f9359ed967e99479505327a4a1a1dda016985e
laciecoder <pvrash1256@gmail.com>
2024-11-03 02:44:25 +0530

    port forward enabled

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
 import { app } from "./app.js";
 import { connectDB } from "./lib/connection.js";
 
-
-const port = parseInt(process.env.PORT || "5000", 10);
+const port = process.env.PORT || 5000;
 
 app.get("/health", (_, res): any => {
-    return res.json({ message: "Healthy" })
-})
+  return res.json({ message: "Healthy" });
+});
 
-connectDB().then(() => {
+connectDB()
+  .then(() => {
     app.listen(port, () => {
-        console.log("Server started at http://localhost:%i", port);
-    })
-}).catch(err => console.log(err));
+      console.log("Server started at http://localhost:%i", port);
+    });
+  })
+  .catch((err) => console.log(err));
