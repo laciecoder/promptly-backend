550bdee5aa986e665ed3a19c798d3bf8df0710a8
laciecoder <pvrash1256@gmail.com>
2024-11-03 03:07:45 +0530

    cors url update

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 config();
 const app = express();
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: "https://promptly-frontend-pi.vercel.app" }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(morgan("dev"));
